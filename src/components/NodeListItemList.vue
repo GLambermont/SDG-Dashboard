6d6f18ae975d063cafb903f9e50ca764952a66a1
Gilbert <gilbertlambermont@gmail.com>
2019-01-21 22:43:53 +0100

    Added node list item for list view

---
+++ src/components/NodeListItemList.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="node-list-item list">
+    box-shadow
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'NodeListItemList'  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/SensorSelect.vue
+++ src/views/SensorSelect.vue
@@ -1,39 +1,20 @@
 <template>
-  <div class="dashboard">
-    <div class="page-section">
-      <div class="content-width">
-        <div class="h1">
-          Node- en sensorselectie
-        </div> <br />
-        <div class="h2">
-          Selecteer een sensor om te starten.
-        </div> <br />
-        <div class="h3">
-          Nodes
-        </div>
-        <node-list-component />
-      </div>
-    </div>
-    <div class="page-section">
-      <div class="content-width">
-        <div class="h3">
-          Sensors
-        </div>
-        <sensor-list-component />
-      </div>
-    </div>
+  <div class="sensor-select">
+    <NodeListItemList />
   </div>
 </template>
 
 <script>
-import NodeListComponent from '@/components/NodeList.vue'
-import SensorListComponent from '@/components/SensorList.vue'
+import NodeList from '@/components/NodeList.vue'
+import SensorList from '@/components/SensorList.vue'
+import NodeListItemList from '@/components/NodeListItemList.vue'
 
 export default {
   name: 'SensorSelect',
   components: {
-    NodeListComponent,
-    SensorListComponent
+    NodeList,
+    SensorList,
+    NodeListItemList
   }
 }
 </script>

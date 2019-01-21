22173de82d0112515ba7b5bf57b3863a8ea8e943
Gilbert <gilbertlambermont@gmail.com>
2019-01-21 22:00:48 +0100

    Made clearer names for pages, async load pages

--- src/router.js
+++ src/router.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Dashboard from './views/Dashboard.vue'
-
+  
 Vue.use(Router)
 
 export default new Router({
@@ -14,17 +13,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/sensor-select',
+      name: 'SensorSelect',
+      component: () => import(/* webpackChunkName: "sensorSelect" */ './views/SensorSelect.vue')
     }
   ]
 })
---
+++ src/views/SensorSelect.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="sensor-select">
+    <div class="content-width-l">
+      <div class="title-group">
+        <h1>Node- en sensor selectie</h1>
+        <h2>Selecteer een sensor om te starten</h2>
+      </div>
+      
+      <main class="page-section-container">
+        <div class="page-section">
+          <header class="list-header">
+            <h3>Nodes</h3>
+            <TextInput type="search" placeholder="Zoeken" fill="#fff" />
+          </header>
+          <NodeList />
+        </div>
+        
+        <div class="page-section">
+          <SensorList />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import TextInput from '@/components/TextInput.vue';
+import NodeList from '@/components/NodeList.vue';
+import SensorList from '@/components/SensorList.vue';
+
+export default {
+  name: 'SensorSelect',
+  components: {
+    TextInput,
+    NodeList,
+    SensorList
+  }
+};
+</script>
+
+<style lang="scss">  
+.sensor-select {
+  overflow: auto;
+}
+
+.page-section-container {
+  display: flex;
+}
+
+.page-section {
+  width: 50%;
+  border: 1px solid pink;
+}
+</style>
